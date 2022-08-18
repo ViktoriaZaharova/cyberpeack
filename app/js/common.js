c67d119c76ca533c4f60a6dcebfcf654418254ce
Viktoria <zaharova181114@gmail.com>
2022-08-18 21:10:42 +0300

    add blocks

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,71 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+$('.partners-company-slider').slick({
+    slidesToShow: 8,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 7,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 5,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+            }
+        }
+    ]
 });
+
+// прогрессбар для слайдера
+function setProgress(index) {
+    const calc = ((index + 1) / ($slider.slick('getSlick').slideCount)) * 100;
+
+    $progressBar
+        .css('width', calc + '%')
+        .attr('aria-valuenow', calc);
+
+}
+
+const $slider = $('.partners-company-slider');
+const $progressBar = $('.progress-bg');
+// const $progressBarLabel = $('.timeline__val');
+
+$slider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    setProgress(nextSlide);
+
+});
+
+
+setProgress(0);
+
+
+
+//плавный скролл
+$(document).ready(function () {
+    $('.go_to').click(function (e) {
+        e.preventDefault();
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length != 0) {
+            $('html, body').animate({
+                scrollTop: $(scroll_el).offset().top
+            }, 500);
+        }
+        return false;
+    });
+});
+//плавный скролл end
